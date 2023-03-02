57df18aa32dede1e5b12916aeffe924d180a4836
ozandilah <79375569+ZanDev-Lab@users.noreply.github.com>
2023-03-02 17:59:45 +0700

    add theme

---
+++ .storybook/laracamp.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+    base: "light",
+    brandTitle: "BOOTCAMPREACT",
+    brandUrl: "https://bootcamp-react.netlify.app/",
+    brandImage: "/logo.png",
+    brandTarget: "_self",
+});
